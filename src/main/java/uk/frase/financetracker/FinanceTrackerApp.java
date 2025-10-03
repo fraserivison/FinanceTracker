83a213291c27a7600fe518e05389253d8aa2ac31
Fraser <fraserivison@hotmail.co.uk>
2025-10-03 21:25:41 +0100

    Initial commit - Finance Tracker app

---
+++ src/main/java/uk/frase/financetracker/FinanceTrackerApp.java
@@ -0,0 +1,7 @@
+package uk.frase.financetracker;
+
+public class FinanceTrackerApp {
+    public static void main(String[] args) {
+        System.out.println("Finance Tracker — hello Frase 👋");
+    }
+}
